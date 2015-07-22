e402235adf1a3f8dd174d49aa41098b71174264a
Stéphane Tournié <stephane.tournie@serli.com>
2015-07-22 11:33:04 +0200

    setting examples package to org.che.examples

---
+++ che-tutorial-add-rest-api/src/main/java/org/eclipse/che/examples/addrestapi/server/inject/SampleRestApiModule.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2012-2015 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.che.examples.addrestapi.server.inject;
+
+import org.eclipse.che.examples.addrestapi.server.SampleService;
+import org.eclipse.che.inject.DynaModule;
+
+import com.google.inject.AbstractModule;
+
+@DynaModule
+public class SampleRestApiModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+        bind(SampleService.class);
+    }
+}
