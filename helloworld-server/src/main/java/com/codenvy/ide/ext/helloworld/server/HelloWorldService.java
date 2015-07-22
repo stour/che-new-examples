6dc70ddbed3d56d9c648a005a7998b505104c7ad
Stéphane Tournié <stephane.tournie@serli.com>
2015-07-22 10:50:47 +0200

    service simplification

--- helloworld-server/src/main/java/com/codenvy/ide/ext/helloworld/server/HelloWorldService.java
+++ helloworld-server/src/main/java/com/codenvy/ide/ext/helloworld/server/HelloWorldService.java
@@ -10,10 +10,8 @@
  *******************************************************************************/
 package com.codenvy.ide.ext.helloworld.server;
 
-import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 
 /**
  * Register a RESTful server side component
@@ -22,15 +20,14 @@ import javax.ws.rs.PathParam;
 @Path("hello")
 public class HelloWorldService {
     
-    @Inject
     public HelloWorldService() {
 
     }
     
     @GET
-    @Path("{name}")
-    public String sayHello(@PathParam("name") String name ) {
-       return "Hello, " + name;
+    @Path("world")
+    public String sayHello() {
+       return "Hello world !";
     }
 
 }
